9043754f0ee0c0af258f1202c6fc40d7355233e4
Felice Forby <feliceforby@gmail.com>
2019-03-27 13:42:38 +0000

    Automatically backed up by Learn

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -1,3 +1,43 @@
+import uuid from 'uuid';
+
 export default (state = [], action) => {
-  return state;
+  let index;
+  let quote;
+  let updatedQuote;
+
+  switch (action.type) {
+    case 'ADD_QUOTE':  
+      return [
+        ...state,
+        action.quote
+      ];
+
+    case 'REMOVE_QUOTE':
+      return filteredQuotes = state.filter(quote => quote.id !== action.quoteId);
+
+    case 'UPVOTE_QUOTE':
+      index = state.findIndex(quote => quote.id === action.quoteId);
+      quote = state[index];
+      updatedQuote = {...quote, votes: quote.votes += 1};
+
+      return [
+        ...state.slice(0, index), 
+        updatedQuote,
+        ...state.slice(index + 1)
+      ];
+
+    case 'DOWNVOTE_QUOTE':
+      index = state.findIndex(quote => quote.id === action.quoteId);
+      quote = state[index];
+      updatedQuote = quote.votes > 0 ? {...quote, votes: quote.votes -= 1} : quote;
+
+      return [
+        ...state.slice(0, index), 
+        updatedQuote,
+        ...state.slice(index + 1)
+      ];
+
+    default:
+      return state;
+  }
 }
